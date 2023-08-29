02d481725768fc112141eaee1aefb1c6ef1545d6
Bhavesh <bhaveshjagtap21@gmail.com>
2023-08-29 20:51:56 +0530

    commit:6

--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -7,7 +7,31 @@ from django.http import HttpResponse, JsonResponse
 
 
 # --------------------------------------------------------------------------------------------
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     print(list(movies.values()))
+#     return HttpResponse(list(movies.values()))
+
+
+# --------------------------------------------------------------------------------------------
 def movie_list(request):
     movies = Movie.objects.all()
     print(list(movies.values()))
-    return HttpResponse(list(movies.values()))
+
+    data = {
+        'movies':list(movies.values())
+    }
+
+    return JsonResponse(data)
+
+# --------------------------------------------------------------------------------------------
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+
+    data = {
+        'movie': movie.name,
+        'description': movie.description,
+        'active': movie.active
+    }
+
+    return JsonResponse(data)
