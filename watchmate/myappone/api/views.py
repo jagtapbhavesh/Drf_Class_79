2400e5fd742dd86f8a95b1dc0bb4fa9653e55cba
Bhavesh <bhaveshjagtap21@gmail.com>
2023-08-31 21:01:36 +0530

    commit:10

--- watchmate/myappone/api/views.py
+++ watchmate/myappone/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from myappone.models import Movie
 from myappone.api.serializers import MovieSerializer
+from rest_framework import status
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -39,7 +40,10 @@ def movie_list(request):
 def movie_details(request, pk):
 
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -50,10 +54,12 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
-
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
-        pass
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
     # movie = Movie.objects.get(pk=pk)
     # serializer = MovieSerializer(movie)
